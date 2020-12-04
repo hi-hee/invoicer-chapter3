3b582222a1005f1db6eecc2ec106d39cd74f0275
hi-hee <vitality710@naver.com>
2020-12-04 20:15:10 +0900

    Update create_ebs_env.sh

--- create_ebs_env.sh
+++ create_ebs_env.sh
@@ -10,7 +10,7 @@ fail() {
     exit 1
 }
 
-export AWS_DEFAULT_REGION=${AWS_REGION:-us-east-1}
+export AWS_DEFAULT_REGION=${AWS_REGION:-us-east-2}
 
 datetag=$(date +%Y%m%d%H%M)
 identifier=$(whoami)-invoicer-$datetag
