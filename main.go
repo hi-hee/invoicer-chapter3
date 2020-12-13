0994d3edde445be353b23d206d4b4654ad6d7ddd
hi-hee <vitality710@naver.com>
2020-12-14 08:01:12 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// Test_deployer_6_58
+// Test_deployer_8_00
 // Contributor: Julien Vehent jvehent@mozilla.com [:ulfr]
 
 
